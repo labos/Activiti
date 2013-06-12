dff680ad25e4597334ea36752476a2a6b1c14e2a
Sandro <sandro.pinna@gmail.com>
2013-06-12 14:48:54 +0200

    Added ProjectFormType and Renderer

---
+++ modules/activiti-explorer/src/main/java/org/activiti/explorer/form/BudgetRow.java
@@ -0,0 +1,27 @@
+package org.activiti.explorer.form;
+
+public class BudgetRow {
+	private String id;
+	private String projectName;
+	private double projectAmount;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public double getProjectAmount() {
+		return projectAmount;
+	}
+	public void setProjectAmount(double projectAmount) {
+		this.projectAmount = projectAmount;
+	}
+
+}
