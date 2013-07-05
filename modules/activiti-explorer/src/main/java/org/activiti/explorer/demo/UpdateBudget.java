450765372ab3d6595f61c8d2ddedb6d4970ce165
alberto <alberto@lab02>
2013-07-05 12:32:01 +0200

    Added check amount to prevent budget exceeded and vat to UpdateBudget delegate

--- modules/activiti-explorer/src/main/java/org/activiti/explorer/demo/UpdateBudget.java
+++ modules/activiti-explorer/src/main/java/org/activiti/explorer/demo/UpdateBudget.java
@@ -12,13 +12,16 @@ public class UpdateBudget implements JavaDelegate {
   
   private Expression sourceToUpdate;
   private Expression amountToDecrease;
-  
+  private Expression vatApplied; 
   public void execute(DelegateExecution execution) {
 	  Boolean sourceUpdated = false;
 	  Source source = (Source) sourceToUpdate.getValue(execution);
 	  Long amount = (Long) amountToDecrease.getValue(execution);
+	  Boolean vatIsApplied = (Boolean) vatApplied.getValue(execution);
+	  Double amountToApply;
 	  if(source!=null){
-		  Double newTotal = source.getTotal() - amount;
+		  amountToApply = (vatIsApplied)? amount * (1 + 0.21) : amount;
+		  Double newTotal = source.getTotal() - amountToApply;
 		  source.setTotal(newTotal);
 		  execution.getEngineServices().getBudgetService().saveSource(source);
 		  sourceUpdated = true;
