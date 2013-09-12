8530b8f48d7e35e82dd10d5d24607e1cf48f97bc
alberto <alberto@lab02>
2013-09-12 12:24:56 +0200

    [SERVICE] Added copy-attachments from a subprocess to the parent process
    - added javadelegate and cmd classes
    - updated bpmn process file with a new service task

--- modules/activiti-explorer/src/main/java/org/sr/activiti/explorer/delegates/CopyAttachmentsToParent.java
+++ modules/activiti-explorer/src/main/java/org/sr/activiti/explorer/delegates/CopyAttachmentsToParent.java
@@ -5,28 +5,29 @@ import java.util.List;
 
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
+import org.activiti.engine.impl.cmd.CreateAttachmentCmd;
+import org.activiti.engine.impl.cmd.sr.CopyAttachmentCmd;
 import org.activiti.engine.impl.context.Context;
-import org.activiti.engine.impl.interceptor.CommandContext;
+import org.activiti.engine.impl.interceptor.CommandExecutor;
 import org.activiti.engine.task.Attachment;
 
-public class CopyAttachmentsToParent implements JavaDelegate{
+public class CopyAttachmentsToParent implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		 List<Attachment> attachmentList = new ArrayList<Attachment>();
-		   CommandContext commandContext = Context.getCommandContext();
-		   attachmentList = execution.getEngineServices().getTaskService().getProcessInstanceAttachments(execution.getParentId());
-		 for(Attachment anAttachment : attachmentList){
-			 //update each anAttachment
-			   /* AttachmentEntity updateAttachment = commandContext
-			    	      .getDbSqlSession()
-			    	      .selectById(AttachmentEntity.class, anAttachment.getId());
-			    	    
-			    	    updateAttachment.setName(anAttachment.getName());
-			    	    updateAttachment.setDescription(anAttachment.getDescription());
-			    	    */
-		 }
-		
+		List<Attachment> attachmentList = new ArrayList<Attachment>();
+		CommandExecutor commandExecutor = Context
+				.getProcessEngineConfiguration()
+				.getCommandExecutorTxRequiresNew();
+		attachmentList = execution
+				.getEngineServices()
+				.getTaskService()
+				.getProcessInstanceAttachments(execution.getProcessInstanceId());
+		for (Attachment anAttachment : attachmentList) {
+			commandExecutor.execute(new CopyAttachmentCmd(anAttachment,
+					execution.getParentId()));
+		}
+
 	}
 
 }
