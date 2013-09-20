21b1b0dcf4cf9c5fb64b3a420d5b6a94b71228ab
guido <guido@labos01>
2013-09-20 13:39:44 +0200

    [BPMN] Sub-procedure selection
    - It's now possible to select three sub-procedures (only ABS is
    implemented) from a launcher process
    - Budget updater now updates the ProjectSource items

--- modules/activiti-explorer/src/main/java/org/sr/activiti/explorer/delegates/UpdateBudget.java
+++ modules/activiti-explorer/src/main/java/org/sr/activiti/explorer/delegates/UpdateBudget.java
@@ -1,7 +1,6 @@
 package org.sr.activiti.explorer.delegates;
 
-import org.activiti.engine.budget.Project;
-import org.activiti.engine.budget.ProjectCostItem;
+import org.activiti.engine.budget.ProjectSourceItem;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.Expression;
 import org.activiti.engine.delegate.JavaDelegate;
@@ -11,29 +10,28 @@ import org.activiti.engine.delegate.JavaDelegate;
  */
 public class UpdateBudget implements JavaDelegate {
   
-  private Expression costToUpdate;
+  private Expression sourceToUpdate;
   private Expression amountToDecrease;
-  private Expression vatApplied; 
+  //private Expression vatApplied; 
   public void execute(DelegateExecution execution) {
-	  Boolean costItemUpdated = false;
-	  ProjectCostItem costItem = (ProjectCostItem) costToUpdate.getValue(execution);
+	  Boolean sourceItemUpdated = false;
+//	  ProjectCostItem costItem = (ProjectCostItem) costToUpdate.getValue(execution);
+	  ProjectSourceItem sourceItem = (ProjectSourceItem) sourceToUpdate.getValue(execution);
 	  Long amount = (Long) amountToDecrease.getValue(execution);
-	  Boolean vatIsApplied = (Boolean) vatApplied.getValue(execution);
-	  Double amountToApply = 0.0;
-	  if(costItem!=null){
-		  amountToApply = (vatIsApplied)? amount * (1 + 0.21) : amount;
-		  Double newActual = costItem.getTotal() - amountToApply;
-		  costItem.setActual(newActual);
-		  execution.getEngineServices().getBudgetService().saveProjectCostItem(costItem);
-		  costItemUpdated = true;
+//    Boolean vatIsApplied = (Boolean) vatApplied.getValue(execution);
+//	  Double amountToApply = 0.0;
+	  if(sourceItem!=null){
+//		  amountToApply = (vatIsApplied)? amount * (1 + 0.21) : amount;
+		  Double newActual = sourceItem.getTotal() - amount;
+		  sourceItem.setActual(newActual);
+		  execution.getEngineServices().getBudgetService().saveProjectSourceItem(sourceItem);
+		  sourceItemUpdated = true;
 	  }
-	  Project project = execution.getEngineServices().getBudgetService().createProjectQuery().projectId(costItem.getIdProject()).singleResult();
-	  Double newTotal = project.getTotal() - amountToApply;
-	  project.setTotal(newTotal);
-	  execution.getEngineServices().getBudgetService().saveProject(project);
-	  execution.setVariable("costItemUpdated",costItemUpdated);
+//	  Project project = execution.getEngineServices().getBudgetService().createProjectQuery().projectId(sourceItem.getIdProject()).singleResult();
+//	  Double newTotal = project.getTotal() - amount;
+//	  project.setTotal(newTotal);
+//	  execution.getEngineServices().getBudgetService().saveProject(project);
+	  execution.setVariable("sourceItemUpdated",sourceItemUpdated);
   }
-  
-  
   
 }
